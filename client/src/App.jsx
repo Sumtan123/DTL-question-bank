025e5bf91be0cf8595c8416bb41f800d9f2768c2
Sumtan123 <Class1@gmail.com>
2023-08-16 21:49:47 +0530

    Add navbar and section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="topBar"><h1>Educate</h1></div>
+        <div className="section">
+          <h3>Section 1-Choose the Correct Option</h3>
+          <h3>Level : Easy</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
