93cb1a67237b8a16cdd8ea591174f1247619a439
Varun Shivakumar <varunshivakumar25@gmail.com>
2023-08-26 21:21:17 +0530

    errored

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Correct_ans from './correct_ans/Correct_ans'
 function App() {
 
   return (
     <>
       <div className="container">
         <div className="topBar"><h1>Educate</h1></div>
-        <div className="section">
-          <h3>Section 1-Choose the Correct Option</h3>
-          <h3>Level : Easy</h3>
-        </div>
+        
+          <Correct_ans/>
+      
       </div>
     </>
   )
